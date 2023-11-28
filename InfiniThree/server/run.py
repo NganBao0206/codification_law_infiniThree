5042d7ed8305489f0e714ef6c956afa096282799
Genji-kun <2051052097phat@ou.edu.vn>
2023-11-28 14:35:54 +0700

    [Phat] - pulling code

--- InfiniThree/server/run.py
+++ InfiniThree/server/run.py
@@ -13,9 +13,4 @@ def home():
     return "home"
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+    app.run(host='0.0.0.0')
