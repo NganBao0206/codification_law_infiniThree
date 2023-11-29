693a6ac3b5715fbe1dcf34300ab23d5d504810ac
NganBao0206 <yuu.mmd02@gmail.com>
2023-11-29 22:52:06 +0700

    [Ngan] - reup

--- server/run.py
+++ server/run.py
@@ -10,7 +10,7 @@ app.register_blueprint(codification_indexes_bp)
 
 @app.route('/')
 def home():
-    return "home page"
+    return "test jenkins"
 
 if __name__ == '__main__':
     app.run(host=host)
