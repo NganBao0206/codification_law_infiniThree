9bdd7e76ba3d01e99d05ef5aeffaa10d9f7df191
NganBao0206 <yuu.mmd02@gmail.com>
2023-11-29 13:46:58 +0700

    [Ngan] - server

--- backend/server/server_app/api/codification_indexes_api.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from server_app.dao.codification_index_dao import *
-
-codification_indexes_bp = Blueprint('codification_indexes', __name__)
-
-@codification_indexes_bp.route('/codification-indexes/<int:indexes_id>/children/', methods=['GET'])
-def get_indexes_child(indexes_id):
-    indexes = get_child_indexes_of_index(indexes_id)
-    return jsonify([index.to_dict() for index in indexes])
--- backend/server/unittest/test_api.py
+++
@@ -1,9 +0,0 @@
-from server_app.api.codification_topic_api import get_all_topics
-from server_app.api.codification_sub_topic_api import get_sub_topics_of_topic
-from server_app.api.codification_indexes_api import get_indexes_sub_topic, get_indexes_child
-
-def test_get_all_topics():
-    topics = get_all_topics()
-    print(topics)
-    assert len(topics) == 45  # Kiểm tra số lượng chủ đề trả về
-
