b5d92e9c8e7769b1db9fefb578ce3f71d60f95be
NganBao0206 <yuu.mmd02@gmail.com>
2023-11-30 14:22:42 +0700

    [Ngan] - add log for testing cors

--- server/run.py
+++ server/run.py
@@ -3,6 +3,7 @@ from server_app.api.codification_topic_api import codification_topics_bp
 from server_app.api.codification_sub_topic_api import codification_sub_topics_bp
 from server_app.api.codification_indexes_api import codification_indexes_bp
 from config import HOST
+from flask import request
 
 app.register_blueprint(codification_topics_bp, url_prefix='/api/codification-topics')
 app.register_blueprint(codification_sub_topics_bp, url_prefix='/api/codification-sub-topics')
@@ -11,6 +12,15 @@ app.register_blueprint(codification_indexes_bp, url_prefix='/api/codification-in
 @app.route('/')
 def home():
     return "home page test remove -v"
+
+@app.after_request
+def log_request_info(response):
+    app.logger.debug('URL: %s', request.url)
+    app.logger.debug('Origin: %s', request.headers.get('Origin'))
+    app.logger.debug('Headers: %s', request.headers)
+    app.logger.debug('Body: %s', request.get_data())
+    return response
+
 
 if __name__ == '__main__':
     app.run(host=HOST)
