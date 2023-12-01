fdeee0fb684e399c5393f8da119a9006b6d87305
NganBao0206 <yuu.mmd02@gmail.com>
2023-12-01 16:05:51 +0700

    [Ngan] - register formdata

--- server/server_app/controller/user_controller.py
+++ server/server_app/controller/user_controller.py
@@ -3,10 +3,10 @@ from server_app.dao.user_dao import *
 from flask_jwt_extended import create_access_token, jwt_required
 
 def register():
-    username = request.json.get("username", None)
-    email = request.json.get("email", None)
-    name = request.json.get('name', None)
-    password = request.json.get("password", None)
+    username = request.form.get("username", None)
+    email = request.form.get("email", None)
+    name = request.form.get('name', None)
+    password = request.form.get("password", None)
 
     user = add_user(name = name, username = username, password = password, email = email)
     print(user)
