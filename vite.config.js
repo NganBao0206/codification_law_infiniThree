a80c03fd2b6e980fdf8860336f79997d7c2c5c6c
Genji-kun <2051052097phat@ou.edu.vn>
2023-12-04 10:34:34 +0700

    [Phat] - Update source code

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    mainFields: [],
+    mainFields: ['browser', 'module', 'jsnext:main', 'main'],
   },
 })
